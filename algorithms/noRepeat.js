c71aefdc500df1b11fa51204e37d5590ab48ea9a
Jared E Jackson <jaredjxn15@gmail.com>
2018-06-03 21:19:35 -0400

    added tests for no repeat algorithms

---
+++ algorithms/noRepeat.js
@@ -0,0 +1,12 @@
+// Given an array of strings,
+// return the string that reoccurs most often.
+
+function noRepeat(arr) {
+    if (Array.isArray(arr) && !arr.length) {
+        return "";
+    }
+    // placeholder for test
+    return "string";
+}
+
+module.exports = noRepeat;
--- test/test.js
+++ test/test.js
@@ -1,15 +1,35 @@
 const expect = require('chai').expect;
 const addTwoNumbers = require('../algorithms/addTwoNumbers');
+const noRepeat = require('../algorithms/noRepeat');
 
-describe('addTwoNumbers()', function() {
-    it('should add two numbers', function() {
+describe('addTwoNumbers()', () => {
+    it('should add two numbers', () => {
         // ARRANGE
         let sum1 = 6;
-
         // ACT
         let sum2 = addTwoNumbers(5, 1);
-
         // ASSERT
         expect(sum2).to.be.equal(sum1);
+    });
+    it("Output should be the typeOf number", () => {
+        let output = addTwoNumbers(1,1);
+        expect(output).to.be.a("number");
+    });
+});
+
+describe("noRepeat()", () => {
+    xit("Output should be a string", () => {
+        const arr = ["input"];
+        const output = noRepeat(arr);
+        expect(output).to.be.a("string");
+    });
+    it("Returns an empty string if the input is an empty array", () => {
+        const output = noRepeat([]);
+        expect(output).to.equal('');
+    });
+    xit("Returns the string with the most occurances", () => {
+        const arr = ["test", "hello", "world", "test", "mocha", "chai", "jasmine", "world", "test"];
+        const output = noRepeat(arr);
+        expect(output).to.equal("test");
     });
 });
